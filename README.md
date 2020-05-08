4ea9145e7486615513b95b7c68d2381e3107a396
sebm91 <sebastian.mattmuller@gmail.com>
2020-05-08 16:41:44 +0200

    First Commit adding files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sebastian Mattmüllers Personal Website (mattmuller.ch)
+
+**Sebastian Mattmüller's Personal Website (WORK IN PROGRESS)**
+
+Website based on the Dimension site template, designed by HTML5 UP and created using Gatsby.js.
+
+## Preview
+
+https://mattmuller.ch/ - deployed on FireBase
+
+## Installation
+
+Run `gatsby develop` in the terminal to start the dev site.
+
+## Deployment on Firebase
+https://www.gatsbyjs.org/docs/deploying-to-firebase/
+
+1) `gatsby build`.
+2) `firebase deploy`
---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-diamond"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Sebastian Mattmüller</h1>
+        <p>
+          Hello, i'm Sebastian!<br/> A developer living in beautiful Switzerland.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            Skills
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
